26da57a76b4c6bd86150cc82ece79120eb4d573c
louis <louis@naiaden.nl>
2014-11-12 17:21:48 +0100

    tfidf per year

--- ReadFile.py
+++ ReadFile.py
@@ -11,11 +11,12 @@ class FileReader:
     compound_words = []
     delimiter = ','
 
-    document_frequencies = []
+    document_word_histogram = {}
     document_frequency = {}
 
     def __init__(self, year_documents=False, stop_words=None, compound_words=None, delimiter=','):
         self.delimiter = delimiter
+        self.year_documents = year_documents
         if stop_words is not None:
             with open(stop_words, 'r') as f:
                 for stop_word in f:
@@ -32,35 +33,46 @@ class FileReader:
     def learn_frequencies(self, file, column=None):
         with open(file, 'r') as f:
             reader = csv.reader(f, delimiter=self.delimiter)
-            for doc_nr, row in enumerate(reader):
-                word_count = {}
+            for doc_id, row in enumerate(reader):
 
+                year = row[1]
                 text = WordPunctTokenizer().tokenize(row[column].lower())
+
+                doc_nr = doc_id
+                if self.year_documents:
+                    doc_nr = year
+
+                word_count_per_document = {}
+                if doc_nr not in self.document_word_histogram:
+                    self.document_word_histogram[doc_nr] = word_count_per_document
+                else:
+                    word_count_per_document = self.document_word_histogram[doc_nr]
+
+                nr_words = 0;
                 for word in text:
                     if word not in self.stop_words:
-                        if word not in word_count:
-                            word_count[word] = 1
+                        nr_words += 1
+                        if word not in word_count_per_document:
+                            word_count_per_document[word] = 1
                         else:
-                            word_count[word] += 1
-                        
+                            word_count_per_document[word] += 1
+               
                         if word not in self.document_frequency:
                             self.document_frequency[word] = set()
                             self.document_frequency[word].add(doc_nr)
                         else:
                             self.document_frequency[word].add(doc_nr);
                     
-                self.document_frequencies.append(word_count)
-        print(len(self.document_frequency))
+                self.document_word_histogram[doc_nr] = word_count_per_document
+                #print(self.document_word_histogram)
+        print("PROCESSED: " + str(len(self.document_frequency)) + " types", file=sys.stderr)
                 
     def compute_tfidf(self, column=None, sparse=False, limit=None):
         words_idx = []
         for k in self.document_frequency.keys():
             words_idx.append(k) 
 
-        for doc_nr, document in enumerate(self.document_frequencies):
-            #            if doc_nr > 5:
-            #    break
-
+        for doc_nr, document in self.document_word_histogram.items():
             doc_tf_idfs = []
             for k, v in self.document_frequency.items():
                 tf = 0
@@ -72,21 +84,29 @@ class FileReader:
                     tf = 1 + math.log(f)
                     bla = k
 
-                N = len(self.document_frequencies)
+                N = len(self.document_word_histogram)
                 idf = math.log(N / len(v))
                 doc_tf_idfs.append(tf*idf)
-#                doc_tf_idfs.append([tf, f, bla])
             if sparse:
+                #pass
                 sorted_tf_idfs_pos = [i[0] for i in sorted(enumerate(doc_tf_idfs), key=lambda x:x[1], reverse=True)]
                 sorted_tf_idf_words = []
                 for idx in sorted_tf_idfs_pos:
                     sorted_tf_idf_words.append(words_idx[idx])
+                #print(sorted_tf_idf_words)
+                #print(doc_tf_idfs)
+
                 if limit is None:
-                    print(zip(sorted(doc_tf_idfs,reverse=True),sorted_tf_idf_words))
-                else:
-                    print(str(doc_nr+1) + str(zip(sorted(doc_tf_idfs,reverse=True),sorted_tf_idf_words)[0:limit]))
+                    limit = len(self.document_frequency)
+                
+                tfidf_word_tuples = (zip(sorted(doc_tf_idfs,reverse=True),sorted_tf_idf_words)[0:limit])
+                #print('|'.join(map(str,tfidf_word_tuples)))
+                output_string = ""
+                for (t1,t2) in tfidf_word_tuples:
+                    output_string += str(t1) + "\t" + str(t2) + "\t"
+                print(str(doc_nr) + "\t" + output_string)
             else:
                 print(doc_tf_idfs)
-        print("PROCESSED: " + str(doc_nr+1) + " documents", file=sys.stderr)
+        print("PROCESSED: " + str(len(self.document_word_histogram)) + " documents", file=sys.stderr)
         print("PROCESSED: " + str(len(self.document_frequency)) + " words", file=sys.stderr)
  
--- tfidf4bm.py
+++ tfidf4bm.py
@@ -2,6 +2,6 @@ from ReadFile import FileReader
 input_file = "bla.tsv"
 content_column = 5
 
-reader = FileReader(delimiter='\t', stop_words="stopwords.txt")
+reader = FileReader(year_documents=True,delimiter='\t', stop_words="stopwords.txt")
 reader.learn_frequencies(input_file, column=content_column)
-reader.compute_tfidf(column=content_column, sparse=True, limit=25)
+reader.compute_tfidf(column=content_column, sparse=True, limit=150)
